9246c5711bdf03a8748156ca24be71cf7cfc35cf
BrandonHeat <test@mail.com>
2023-11-08 18:37:26 +0300

    update

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,52 +1,117 @@
-package SimpleGame;
+package game;
 
-import java.util.InputMismatchException;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Game {
+    String userName;
+    int count = 1;
+    int tryToGuess = 0;
+    int randomNumber;
+    int maxNumber;
+    List<String> score = new ArrayList<>();
+    Scanner input = new Scanner(System.in);
 
-
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-        Random r = new Random();
-        int randomNumber = r.nextInt(10);
-        int count = 1;
-        System.out.println("Попробуй угадать число до 10!");
-
+    public void startGame() {
+        System.out.println("Привет! Это игра угадай число! Я загадываю число.... ");
         while (true) {
-            if (count > 1) {
-                System.out.print("Еще раз? Введи ДА или НЕТ!:");
-                input.nextLine();
-            } else {
-                System.out.print("Играем? Введи ДА или НЕТ!:");
-            }
-            String answer = input.nextLine();
-            if (answer.equals("ДА")) {
-                System.out.println("Введи любое число от 0 до 10!");
-                while (true) {
-                    try {
-                        int number = input.nextInt();
-                        if (number < randomNumber) {
-                            System.out.println("Слишком мало!");
-                        } else if (number > randomNumber) {
-                            System.out.println("Слишком много!");
-                        } else {
-                            System.out.println("Правильно!");
-                            count++;
-                            break;
-                        }
-                    } catch (InputMismatchException e) {
-                        System.out.println("Введите корректное число!");
-                        input.next();
-                    }
-                }
-            } else if (answer.equals("НЕТ")) {
-                System.out.println("Хорошо, пока!");
+            try {
+                System.out.println("Введи максимальное число которое я могу загодать :");
+                maxNumber = input.nextInt();
                 break;
+            } catch (InputMismatchException i) {
+                System.out.println("Введите корректное число!");
+                input.next();
             }
         }
 
+            System.out.println("Попробуешь угадать?");
+
+            while (true) {
+                randomNumber = (int) (Math.random() * maxNumber);
+                if (count > 1) {
+                    System.out.print("Еще раз? Да/Нет? - ");
+                    input.nextLine();
+                } else {
+                    System.out.print("Да/Нет? - ");
+                }
+                String answer = input.nextLine();
+                if (answer.equalsIgnoreCase("ДА") && count == 1) {
+                    System.out.print("Введи никнейм -  ");
+                    userName = input.nextLine();
+                    System.out.println("Введи любое число от 0 до " + maxNumber);
+                    while (true) {
+                        try {
+                            int number = input.nextInt();
+                            if (number < randomNumber) {
+                                System.out.println("Слишком мало!");
+                                count++;
+                            } else if (number > randomNumber) {
+                                System.out.println("Слишком много!");
+                                count++;
+                            } else {
+                                System.out.println("Правильно!");
+                                System.out.println(userName + " угадал число c " + count + " раза!");
+                                tryToGuess = tryToGuess + 1;
+                                score.add("Попытка №" + tryToGuess + ":" + "Пользователь"  + "[" + userName + "]" + ":" + "Угадал цифру" + "[" + maxNumber + "]" + " c " + count + " раз(а);");
+                                count++;
+                                break;
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Введите корректное число!");
+                            input.next();
+                        }
+                    }
+                } else if (answer.equalsIgnoreCase("ДА") && count > 1) {
+                    System.out.print("Хочешь сменить никнейм? Да/Нет? -  ");
+                    String changeNickName = input.nextLine();
+                    count = 1;
+                    if (changeNickName.equalsIgnoreCase("Да")) {
+                        System.out.print("Введи новый никнейм -  ");
+                        userName = input.nextLine();
+                        System.out.println("Введи любое число от 0 до " + maxNumber);
+                    } else {
+                        System.out.println("Введи любое число от 0 до " + maxNumber);
+                    }
+                    while (true) {
+                        try {
+                            int number = input.nextInt();
+                            if (number < randomNumber) {
+                                System.out.println("Слишком мало!");
+                                count++;
+                            } else if (number > randomNumber) {
+                                System.out.println("Слишком много!");
+                                count++;
+                            } else {
+                                System.out.println("Правильно!");
+                                System.out.println(userName + " угадал число c " + count + " раза!");
+                                tryToGuess = tryToGuess + 1;
+                                score.add("Попытка №" + tryToGuess + ":" + "Пользователь"  + "[" + userName + "]" + ":" + "Угадал цифру" + "[" + maxNumber + "]" + " c " + count + " раз(а);");
+                                count++;
+                                break;
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Введите корректное число!");
+                            input.next();
+                        }
+                    }
+                } else if (answer.equalsIgnoreCase("НЕТ")) {
+                    System.out.println("Показать статистику игр? Да/Нет?");
+                    String showScore = input.nextLine();
+                    if (showScore.equalsIgnoreCase("да")) {
+                        for(String i:score){
+                            System.out.println(i);
+                        }
+                        System.out.println("Спасибо за игру, удачи!");
+                        break;
+                    } else if (showScore.equalsIgnoreCase("нет")) {
+                        System.out.println("Хорошо, удачи!");
+                        break;
+                    }
+                }
+            }
+
+
     }
+
 }
+
---
+++ src/game/GameLauncher.java
@@ -0,0 +1,15 @@
+package game;
+
+public class GameLauncher {
+
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.startGame();
+
+
+
+    }
+
+}
